354ed93380880e1b0f3fc2c46eca0807f69a073d
Heryson Andrade <141951314+ribshow@users.noreply.github.com>
2024-12-06 17:34:32 -0300

    fix(blacklist)

--- Controllers/BlacklistController.cs
+++ Controllers/BlacklistController.cs
@@ -13,11 +13,17 @@ namespace chatApi.Controllers
         private readonly ContextMongoDb _database;
 
         private readonly ChatService _chatService;
+        private readonly ChatTechService _chatTechService;
+        private readonly ChatGeekService _chatGeekService;
+        private readonly ChatSciService _chatSciService;
 
-        public BlacklistController(ContextMongoDb database, ChatService chatService)
+        public BlacklistController(ContextMongoDb database, ChatService chatService, ChatTechService chatTechService, ChatGeekService chatGeekService, ChatSciService chatSciService)
         {
             _database = database;
             _chatService = chatService;
+            _chatTechService = chatTechService;
+            _chatGeekService = chatGeekService;
+            _chatSciService = chatSciService;
         }
 
         [HttpGet("/report/all")]
@@ -32,30 +38,83 @@ namespace chatApi.Controllers
         /// Report Message from ChatHub
         /// </summary>
         /// <param name="id"></param>
+        /// <param name="origin"></param>
         /// <returns>Successful report</returns>
         /// <response code="200">Successful report</response>
         /// <responde code="404">Message not found</responde>
         [HttpPost("report/chathub")]
         [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(Response))]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
-        public async Task<IActionResult> ReportMessage([FromForm] string id)
+        public async Task<IActionResult> ReportMessage([FromForm] string id, [FromForm]string origin)
         {
             if (string.IsNullOrEmpty(id))
             {
                 return BadRequest();
             }
 
-            var chat = await _chatService.GetAsync(id);
-
-            if (chat == null)
+            // Verificando a origem da mensagem e buscando ela na coleção apropriada
+            if(origin == "ChatHub")
             {
-                return NotFound();
+                var chat = await _chatService.GetAsync(id);
+                if(chat == null)
+                {
+                    return NotFound(new {
+                        status = 404,
+                        error = "Chat message not found!"
+                    });
+                }
+                Blacklist blacklist = new(chat.Id, chat.Fullname, chat.Nickname, chat.Message, chat.date, origin);
+                await _database.Blacklist.InsertOneAsync(blacklist);
             }
-            Console.WriteLine($"{chat.Message}");
 
-            Blacklist blacklist = new(chat.Id, chat.Fullname, chat.Nickname, chat.Message, chat.date, "ChatHub");
+            // Verificando a origem da mensagem e buscando ela na coleção apropriada
+            if (origin == "ChatTech")
+            {
+                var chat = await _chatTechService.GetAsync(id);
+                if (chat == null)
+                {
+                    return NotFound(new
+                    {
+                        status = 404,
+                        error = "Chat message not found!"
+                    });
+                }
+                Blacklist blacklist = new(chat.Id, chat.Fullname, chat.Nickname, chat.Message, chat.date, origin);
+                await _database.Blacklist.InsertOneAsync(blacklist);
+            }
 
-            await _database.Blacklist.InsertOneAsync(blacklist);
+            // Verificando a origem da mensagem e buscando ela na coleção apropriada
+            if (origin == "ChatGeek")
+            {
+                var chat = await _chatGeekService.GetAsync(id);
+                if (chat == null)
+                {
+                    return NotFound(new
+                    {
+                        status = 404,
+                        error = "Chat message not found!"
+                    });
+                }
+                Blacklist blacklist = new(chat.Id, chat.Fullname, chat.Nickname, chat.Message, chat.date, origin);
+                await _database.Blacklist.InsertOneAsync(blacklist);
+            }
+
+            // Verificando a origem da mensagem e buscando ela na coleção apropriada
+            if (origin == "ChatSci")
+            {
+                var chat = await _chatSciService.GetAsync(id);
+                if (chat == null)
+                {
+                    return NotFound(new
+                    {
+                        status = 404,
+                        error = "Chat message not found!"
+                    });
+                }
+                Blacklist blacklist = new(chat.Id, chat.Fullname, chat.Nickname, chat.Message, chat.date, origin);
+                await _database.Blacklist.InsertOneAsync(blacklist);
+            }
+             
 
 
             return Ok(new Response { Message = "Successful report!" });
